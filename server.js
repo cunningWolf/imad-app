00bd79dff0db7dee917d52c72ff69fc2031a41d3
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-12 13:10:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,78 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One by GB',
+    heading: 'Article One',
+    date: 'August 12 2017',
+    content: `<p>
+                            My first article .My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article My first article 
+                        </p>
+                        <p>
+                            My first article .My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article My first article 
+                        </p>
+                        <p>
+                            My first article .My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article My first article 
+                        </p>
+                        <p>
+                            My first article .My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article 
+                            My first article My first article My first article My first article My first article My first article My first article 
+                        </p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+<html>
+    
+    <head>
+        
+        <title>
+          ${heading}
+        </title>
+        <meta name = "viewport" content = "width=device-width, initial-scale=1"> 
+        <link href="/ui/style.css" rel="stylesheet" />
+        <style>
+           
+        </style>
+    </head>
+    <body>
+        <div class="container">
+                    <div>
+                        <a href = "/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                       ${content}
+                    </div>
+         </div>
+        
+    </body>
+    
+</html>`
+
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(data));
 });
 
 app.get('/article-one', function (req, res) {
