5ae04a89e6405c241dd0567c3478c4d10e3efc5b
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-12 14:56:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,16 +139,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.param.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/:articleName', function (req, res) {
-    var articleName = req.param.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
