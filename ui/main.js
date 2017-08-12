a2a72136ab8fd5cb67f9390fcc3054521fd33331
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-12 15:46:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,16 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+//moving element
+
+var img = document.getElementById("madi");
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft +'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveLeft,50);
+}
