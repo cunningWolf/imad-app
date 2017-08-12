aaad26f8098558e6dbb4ef67705586e9ca85dee2
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-12 23:14:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,8 @@
 var button = document.getElementById('counter');
 var counter = 0;
 
+
+
 button.onclick = function(){
     
     var request = new XMLHttpRequest();
@@ -37,4 +39,23 @@ submit.onclick = function(){
     
     var ul = document.getElementById('namelist')
     ul.innerHTML = list;
+};
+
+submit.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange =  function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter = request.responseText;
+                var namelist = document.getElementById('namelist');
+                namelist.innerHTML = counter.toString();
+            }
+        }
+    }
+    
+    request.open('GET','http://gauravtcs15.imad.hasura-app.io/submit-name'+name,true);
+    request.send(null);
+    
 };
