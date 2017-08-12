49cbc885c63e4fa0b0cb14a5f5df1f8a87e1443d
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-12 23:17:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,9 +48,16 @@ submit.onclick = function(){
     request.onreadystatechange =  function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
-                var counter = request.responseText;
-                var namelist = document.getElementById('namelist');
-                namelist.innerHTML = counter.toString();
+                var names = request.responseText;
+                 names = JSON.parse(names);
+                 var list = '';
+    
+                for(var i=0;i<names.length;i++){
+                   list += '<li>'+names[i]+'</li>';
+                 }
+    
+                var ul = document.getElementById('namelist')
+                ul.innerHTML = list;
             }
         }
     }
