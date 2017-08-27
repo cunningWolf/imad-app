ddcad1d6b0dc8d3d63bffc1cda49490cf7c7477a
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-08-27 11:35:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,9 +172,25 @@ app.get('/test-db',function(req,res){
     })
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    
+    
+    
+    pool.query("select * from article where title = "+req.params.articleName,function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           if(result.row.length === 0){
+               req.status(404).send('Article not Found');
+           }
+           else{
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+   
 });
 
 app.get('/ui/style.css', function (req, res) {
