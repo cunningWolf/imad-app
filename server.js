513aafc640b9ae6eae9535e9364edf8455e90506
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-09-04 16:40:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ app.post('/login',function(req,res){
        } 
        else{
            if( result.rows.length === 0){
-               res.send(403).send('username/password is invalid');
+               res.status(403).send('username/password is invalid');
            }
            else{
                 var dbString = result.rows[0].password;
