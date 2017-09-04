834d15ca88cce2a100b7b70fe8a7f36fdd3c3f28
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-09-04 13:31:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,5 @@
 /// increase counter values 
 
-var button = document.getElementById('counter');
-var counter = 0;
-
-
-
-button.onclick = function(){
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange =  function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    }
-    
-    request.open('GET','http://gauravtcs15.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
-
-
-
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -34,22 +8,21 @@ submit.onclick = function(){
     request.onreadystatechange =  function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
-                var names = request.responseText;
-                 names = JSON.parse(names);
-                 var list = '';
-    
-                for(var i=0;i<names.length;i++){
-                   list += '<li>'+names[i]+'</li>';
-                 }
-    
-                var ul = document.getElementById('namelist')
-                ul.innerHTML = list;
+                alert('Logged in successfully!');
+            }else if(request.status === 403){
+                alert('username or password is incorrect');
+            }else if(request.status === 500){
+                alert('Something went wrong in the server');
             }
         }
     }
-    var rawInput = document.getElementById('name');
-    var name = rawInput.value;
-    request.open('GET','http://gauravtcs15.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
     
+    var username  = document.getElementById('username').value;
+    var password  = document.getElementById('password').value;
+    
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://gauravtcs15.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
 };
