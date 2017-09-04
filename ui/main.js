5ce4e023c85e6a59d007c50b72b174df56035169
gaurav bajpai <gaurav.tcs15@gmail.com>
2017-09-04 16:42:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,5 +24,5 @@ submit.onclick = function(){
     console.log(password);
     request.open('POST','http://gauravtcs15.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({"username": username,"password": password}));
+    request.send(JSON.stringify({username: username,password: password}));
 };
